bfb1080760fb49cde7c172d38ced23f3af2c9385
Muhammadali Sulton <mukhammadalimk@gmail.com>
2024-08-27 14:38:04 +0900

    initial commit

---
+++ components/shared/RightSidebar.tsx
@@ -0,0 +1,101 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import RenderTag from "./RenderTag";
+
+const RightSidebar = () => {
+  const hotQuestions = [
+    {
+      _id: "1",
+      title:
+        "Best practices for data fetching in a Next.js application with Server-Side Rendering (SSR)?",
+    },
+    {
+      _id: "2",
+      title: "Is it only me or the font is bolder than necessary?",
+    },
+    {
+      _id: "3",
+      title: "Redux Toolkit Not Updating State as Expected",
+    },
+    {
+      _id: "4",
+      title: "Can I get the course for free?",
+    },
+    {
+      _id: "5",
+      title: "Async/Await Function Not Handling Errors Properly",
+    },
+  ];
+
+  const popularTags = [
+    {
+      _id: "1",
+      name: "javasccript",
+      totalQuestions: 5,
+    },
+    {
+      _id: "2",
+      name: "react",
+      totalQuestions: 4,
+    },
+    {
+      _id: "3",
+      name: "next",
+      totalQuestions: 3,
+    },
+    {
+      _id: "4",
+      name: "vue",
+      totalQuestions: 5,
+    },
+    {
+      _id: "5",
+      name: "redux",
+      totalQuestions: 2,
+    },
+  ];
+
+  return (
+    <section className="background-light900_dark200 light-border custom-scrollbar sticky right-0 top-0 flex h-screen  w-[350px] flex-col overflow-y-auto border-l p-6 pt-36 shadow-light-300 dark:shadow-none max-xl:hidden">
+      <div>
+        <h3 className="h3-bold text-dark200_light900">Top Questions</h3>
+        <div className="mt-7 flex w-full flex-col gap-[20px]">
+          {hotQuestions.map((item) => (
+            <Link
+              key={item._id}
+              href={`/questions/${item._id}`}
+              className="flex cursor-pointer items-center justify-between gap-7"
+            >
+              <p className="body-medium text-dark500_light700">{item.title}</p>
+              <Image
+                src="/assets/icons/chevron-right.svg"
+                alt="chevron right"
+                width={20}
+                height={20}
+                className="invert-colors"
+              />
+            </Link>
+          ))}
+        </div>
+      </div>
+      <div className="mt-14">
+        <h3 className="h3-bold text-dark200_light900">Popular tags</h3>
+
+        <div className="mt-7 flex flex-col gap-4">
+          {popularTags.map((tag) => (
+            <RenderTag
+              key={tag._id}
+              _id={tag._id}
+              name={tag.name}
+              totalQuestions={tag.totalQuestions}
+              showCount
+            />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default RightSidebar;
---
+++ lib/actions/question.action.ts
@@ -0,0 +1,96 @@
+"use server";
+
+import Question from "@/database/question.model";
+import { connectToDatabase } from "../mongoose";
+import Tag from "@/database/tag.model";
+import User from "@/database/user.model";
+import { FilterQuery } from "mongoose";
+import { CreateQuestionParams, GetQuestionsParams } from "./shared.types";
+import { revalidatePath } from "next/cache";
+
+export async function getQuestions(params: GetQuestionsParams) {
+  try {
+    connectToDatabase();
+
+    const { searchQuery, filter, page = 1, pageSize = 10 } = params;
+
+    // Calculcate the number of posts to skip based on the page number and page size
+    const skipAmount = (page - 1) * pageSize;
+
+    const query: FilterQuery<typeof Question> = {};
+
+    if (searchQuery) {
+      const escapedSearchQuery = searchQuery.replace(
+        /[.*+?^${}()|[\]\\]/g,
+        "\\$&"
+      );
+      query.$or = [
+        { title: { $regex: new RegExp(escapedSearchQuery, "i") } },
+        { content: { $regex: new RegExp(escapedSearchQuery, "i") } },
+      ];
+    }
+
+    let sortOptions = {};
+
+    switch (filter) {
+      case "newest":
+        sortOptions = { createdAt: -1 };
+        break;
+      case "frequent":
+        sortOptions = { views: -1 };
+        break;
+      case "unanswered":
+        query.answers = { $size: 0 };
+        break;
+      default:
+        break;
+    }
+
+    const questions = await Question.find(query)
+      .populate({ path: "tags", model: Tag })
+      .populate({ path: "author", model: User })
+      .skip(skipAmount)
+      .limit(pageSize)
+      .sort(sortOptions);
+
+    const totalQuestions = await Question.countDocuments(query);
+
+    const isNext = totalQuestions > skipAmount + questions.length;
+
+    return { questions, isNext };
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+
+export async function createQuestion(params: CreateQuestionParams) {
+  try {
+    connectToDatabase();
+
+    const { title, content, tags, author, path } = params;
+
+    // Create the question
+
+    const question = await Question.create({ title, content, author });
+
+    const tagDocuments = [];
+
+    // Create the tags or get them if they already exist
+    for (const tag of tags) {
+      const existingTag = await Tag.findOneAndUpdate(
+        { name: { $regex: new RegExp(`^${tag}$`, "i") } },
+        { $setOnInsert: { name: tag }, $push: { questions: question._id } },
+        { upsert: true, new: true }
+      );
+
+      tagDocuments.push(existingTag._id);
+    }
+
+    await Question.findByIdAndUpdate(question._id, {
+      $push: { tags: { $each: tagDocuments } },
+    });
+
+    revalidatePath(path);
+  } catch (error) {}
+}
