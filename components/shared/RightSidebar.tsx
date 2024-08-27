38c8e460264364d6467118c8992185af62a7817b
Muhammadali Sulton <mukhammadalimk@gmail.com>
2024-08-28 03:09:35 +0900

    added top results

--- components/shared/RightSidebar.tsx
+++ components/shared/RightSidebar.tsx
@@ -2,59 +2,12 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import RenderTag from "./RenderTag";
+import { getHotQuestions } from "@/lib/actions/question.action";
+import { getTopPopularTags } from "@/lib/actions/tag.actions";
 
-const RightSidebar = () => {
-  const hotQuestions = [
-    {
-      _id: "1",
-      title:
-        "Best practices for data fetching in a Next.js application with Server-Side Rendering (SSR)?",
-    },
-    {
-      _id: "2",
-      title: "Is it only me or the font is bolder than necessary?",
-    },
-    {
-      _id: "3",
-      title: "Redux Toolkit Not Updating State as Expected",
-    },
-    {
-      _id: "4",
-      title: "Can I get the course for free?",
-    },
-    {
-      _id: "5",
-      title: "Async/Await Function Not Handling Errors Properly",
-    },
-  ];
-
-  const popularTags = [
-    {
-      _id: "1",
-      name: "javasccript",
-      totalQuestions: 5,
-    },
-    {
-      _id: "2",
-      name: "react",
-      totalQuestions: 4,
-    },
-    {
-      _id: "3",
-      name: "next",
-      totalQuestions: 3,
-    },
-    {
-      _id: "4",
-      name: "vue",
-      totalQuestions: 5,
-    },
-    {
-      _id: "5",
-      name: "redux",
-      totalQuestions: 2,
-    },
-  ];
+const RightSidebar = async () => {
+  const hotQuestions = await getHotQuestions();
+  const popularTags = await getTopPopularTags();
 
   return (
     <section className="background-light900_dark200 light-border custom-scrollbar sticky right-0 top-0 flex h-screen  w-[350px] flex-col overflow-y-auto border-l p-6 pt-36 shadow-light-300 dark:shadow-none max-xl:hidden">
@@ -64,7 +17,7 @@ const RightSidebar = () => {
           {hotQuestions.map((item) => (
             <Link
               key={item._id}
-              href={`/questions/${item._id}`}
+              href={`/question/${item._id}`}
               className="flex cursor-pointer items-center justify-between gap-7"
             >
               <p className="body-medium text-dark500_light700">{item.title}</p>
