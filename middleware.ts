6ff1c405bf355029cf5fda5321398961351e1031
Muhammadali Sulton <mukhammadalimk@gmail.com>
2024-08-27 17:24:47 +0900

    updated middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,37 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
+// Define your public and ignored routes
+const publicRoutes = [
+  "/",
+  "/api/webhook",
+  "/question/:id",
+  "/tags",
+  "/tags/:id",
+  "/profile/:id",
+  "/community",
+  "/jobs",
+];
+const ignoredRoutes = ["/api/webhook", "/api/chatgpt"];
+
 const isProtectedRoute = createRouteMatcher(["/ask-question(.*)"]);
 
 export default clerkMiddleware((auth, req) => {
-  if (isProtectedRoute(req)) auth().protect();
+  const url = req.nextUrl.pathname;
+
+  // Check if the route is in ignoredRoutes
+  if (ignoredRoutes.some((route) => url.startsWith(route))) {
+    return; // Skip the middleware for ignored routes
+  }
+
+  // Check if the route is in publicRoutes
+  if (publicRoutes.some((route) => url.startsWith(route))) {
+    return; // Allow access to public routes without authentication
+  }
+
+  // Protect the route if it's a protected route
+  if (isProtectedRoute(req)) {
+    auth().protect();
+  }
 });
 
 export const config = {
